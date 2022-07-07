27e6a4fb8f227b3209a2c3e71b54d89a312d9ebb
Sheshkon <sheskoalexei@gmail.com>
2022-07-07 13:27:39 +0300

    added test for pages

--- innotter/tests/conftest.py
+++ innotter/tests/conftest.py
@@ -5,6 +5,8 @@ from rest_framework.test import APIClient
 
 from dataclasses import dataclass
 
+from pages.models import Tag, Post, Page, Like
+
 
 @dataclass
 class Tester:
@@ -77,3 +79,108 @@ def auth_admin_client(client, admin):
     access_token = response.data.get('access_token', None)
     client.credentials(HTTP_AUTHORIZATION='Token ' + access_token)
     return client
+
+
+@pytest.fixture
+def user_page(user):
+    return Page.objects.create(
+        name='user_page',
+        uuid='user_tester',
+        description='admin user page description',
+        owner=user,
+        is_private=False,
+    )
+
+
+@pytest.fixture
+def admin_page(admin):
+    return Page.objects.create(
+        name='admin_page',
+        uuid='admin_tester',
+        description='admin user page description',
+        owner=admin,
+        is_private=False,
+    )
+
+
+@pytest.fixture
+def user_page_with_follower(user_page, admin):
+    user_page.followers.add(admin)
+    user_page.save()
+    return user_page
+
+
+@pytest.fixture
+def admin_page_with_follower(admin_page, user):
+    admin_page.followers.add(user)
+    admin_page.save()
+    return admin_page
+
+
+@pytest.fixture
+def private_user_page(user_page):
+    user_page.is_private = True
+    user_page.save()
+    return user_page
+
+
+@pytest.fixture
+def private_admin_page(admin_page):
+    admin_page.is_private = True
+    admin_page.save()
+    return admin_page
+
+
+@pytest.fixture
+def user_private_page_with_follow_request(private_user_page, admin):
+    private_user_page.follow_requests.add(admin)
+    private_user_page.save()
+    return private_user_page
+
+
+@pytest.fixture
+def admin_private_page_with_follow_request(private_admin_page, user):
+    private_admin_page.follow_requests.add(user)
+    private_admin_page.save()
+    return private_admin_page
+
+
+@pytest.fixture
+def tags_list():
+    tags = []
+    for i in range(5):
+        tags.append(Tag.objects.create(name=str(i)))
+    return tags
+
+
+@pytest.fixture
+def user_page_with_tags(user_page, tags_list):
+    user_page.tags.add(*Tag.objects.all())
+    return user_page
+
+
+@pytest.fixture
+def user_post(user, user_page):
+    return Post.objects.create(
+        page=user_page,
+        content='test user post content'
+    )
+
+
+@pytest.fixture
+def admin_post(user, admin_page):
+    return Post.objects.create(
+        page=admin_page,
+        content='test admin post content'
+    )
+
+
+@pytest.fixture
+def likes(user, admin, user_post):
+    return (Like.objects.create(owner=user, post=user_post),
+            Like.objects.create(owner=admin, post=user_post))
+
+
+
+
+
