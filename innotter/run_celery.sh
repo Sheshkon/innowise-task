de0599ebc10cba1d4464d68fd645771ca6e790c1
Sheshkon <sheskoalexei@gmail.com>
2022-07-22 16:01:49 +0300

    fix: start web and worker after localstack response, create s3 bucket when container up

--- innotter/run_celery.sh
+++ innotter/run_celery.sh
@@ -1,1 +1,20 @@
-celery -A innotter worker -l INFO
+#!/bin/sh
+echo 'start celery worker'
+MAX_TRIES_CONNECTIONS=5
+sleep 10
+result=$(curl -s "$LOCALSTACK_ENDPOINT_URL")
+tries_connections=0
+
+while [ "$result" != '{"status": "running"}' ] && [ "$tries_connections" != "$MAX_TRIES_CONNECTIONS" ]
+do
+  result=$(curl -s "$LOCALSTACK_ENDPOINT_URL")
+  tries_connections=$((tries_connections+1))
+  sleep 5
+done
+
+if [ "$tries_connections" = "$MAX_TRIES_CONNECTIONS" ]; then
+  echo 'failed start worker: cannot connect to aws services'
+else
+  celery -A innotter worker -l INFO
+  echo 'celery worker started'
+fi
